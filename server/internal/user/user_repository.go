45f47ca1cdede0c8d77bf2330f11b3e085ae0314
suyogkokaje <suyogkokaje6@gmail.com>
2023-10-12 16:11:18 +0530

    feat:jwt auth

--- server/internal/user/user_repository.go
+++ server/internal/user/user_repository.go
@@ -12,21 +12,37 @@ type DBTX interface {
 	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
 }
 
-type repository struct{
+type repository struct {
 	db DBTX
 }
 
-func NewRepository(db DBTX) Repository{
-	return &repository{db:db}
+func NewRepository(db DBTX) Repository {
+	return &repository{db: db}
 }
 
-func (r *repository) CreateUser(ctx context.Context, user *User) (*User,error){
+func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
 	var lastInsertId int
+
 	query := "INSERT INTO users(username,password,email) VALUES ($1,$2,$3) returning id"
-	err:=r.db.QueryRowContext(ctx,query,user.Username,user.Password,user.Email).Scan(&lastInsertId)
-	if err!=nil{
-		return &User{},err
+	err := r.db.QueryRowContext(ctx, query, user.Username, user.Password, user.Email).Scan(&lastInsertId)
+
+	if err != nil {
+		return &User{}, err
 	}
+
 	user.ID = int64(lastInsertId)
-	return user,nil
+	return user, nil
+}
+
+func (r *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
+	u := User{}
+
+	query := "SELECT id, email, username, password FROM users WHERE email = $1"
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+
+	if err != nil {
+		return &User{}, err
+	}
+
+	return &u, nil
 }
--- server/router/router.go
+++ server/router/router.go
@@ -11,6 +11,8 @@ var r *gin.Engine
 func  InitRouter(userHandler *user.Handler){
 	r = gin.Default()
 	r.POST("/signup",userHandler.CreateUser)
+	r.POST("/login",userHandler.Login)
+	r.GET("/logout",userHandler.Logout)
 }
 
 
