c9f570a49530942a306d846caacc16d2c6bfca1c
suyogkokaje <suyogkokaje6@gmail.com>
2023-10-12 15:30:00 +0530

    feat:signup endpoint

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main(){
-	_,err:=db.NewDatabase()
+	dbConn,err:=db.NewDatabase()
 	if err!=nil{
 		log.Fatalf("Failed to initialize database connection: %s",err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHanlder(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
